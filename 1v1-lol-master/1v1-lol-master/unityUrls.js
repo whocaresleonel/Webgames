6b7ec83dd85aeb764bcf09c2507cad8c74ae58f2
whocaresleonel <lhirmiz2@gmail.com>
2024-10-21 19:25:47 +1100

    Add files via upload

---
+++ 1v1-lol-master/1v1-lol-master/unityUrls.js
@@ -0,0 +1,9 @@
+window.loadedUrls = true;
+
+function getBuildUrl() {
+	return "https://justplay-cdn.playtika.com/justbuild/NewCI/1v1/Prod/404/19f663180e6f4a21a8c580253f83ca9b";
+}
+
+function getGameLoaderUrl() {
+	return getBuildUrl() + "/WebGL.loader.js";
+}
